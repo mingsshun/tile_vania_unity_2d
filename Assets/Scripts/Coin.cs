ec9d54a74a34ffcc4722f18fe0b6b9d9b9e0574a
min.shuun <min.shuun@gmail.com>
2023-05-30 23:57:30 +0700

    31. Coin Sound Effect

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    [SerializeField] AudioClip CoinPickupSFX;
     void OnTriggerEnter2D(Collider2D other) {
         if(other.tag == "Player")
         {
+            AudioSource.PlayClipAtPoint(CoinPickupSFX, Camera.main.transform.position);
             Destroy(gameObject);
         }
     }
